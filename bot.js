f351482139d401fc97cb712617364e5c9740c67e
ezookik <45100686+bankroom@users.noreply.github.com>
2018-12-01 14:12:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "510089379281371137"; // ايدي السررفر
+var channel = "516662593419477012";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik , ezookik ,
  **')
